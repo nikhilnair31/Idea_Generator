b6c6480c28d3225f860f0c12228c0734fee82367
Nikhil Nair <niknair3189@gmail.com>
2021-06-02 19:53:49 +0530

    idk

--- idea_gen_gpt2_test.py
+++ idea_gen_gpt2_test.py
@@ -2,6 +2,7 @@ import json
 import time
 import random
 import pandas as pd
+import tensorflow as tf
 from transformers import pipeline
 from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
 import firebase_admin
@@ -12,15 +13,16 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 model_paths = ['./models/Startup_Ideas_model', './models/AppIdeas_model', './models/gameideas_model']
 model_name = 'gpt2-medium' # gpt2, gpt2-medium, 
-max_len = 100
-num_return_sequences = 1
+max_len = 150
+num_return_sequences = 10
 
-def get_idea():
+def game_idea_pipeline():
     idea_gen = pipeline('text-generation', model=model_path, tokenizer=model_name, config={'max_length':max_len})
     return idea_gen('<|title|>', max_length=max_len, num_return_sequences=num_return_sequences)
 
-def game_idea():
-    model_path = random.choice(model_paths)
+def game_idea_generate():
+    # model_path = random.choice(model_paths)
+    model_path = model_paths[0]
     model = TFGPT2LMHeadModel.from_pretrained(model_path, from_pt=True)
     tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     input_ids = tokenizer.encode("<|title|>", add_special_tokens=True, return_tensors='tf')
@@ -38,7 +40,7 @@ def game_idea():
         print(f'{i}: {decoded_idea[9:]}\n')
 
 def firestore_push(idea_text):
-    doc_ref = db.collection('posts').add({
+    doc_ref = db.collection('generated').add({
         'displayName': 'GPT2-Bot', 
         'uid': 'DFUxm8vnMgGKBh6AsZCcjXpek57H', 
         'idea': idea_text, 
@@ -47,4 +49,4 @@ def firestore_push(idea_text):
     })
 
 if __name__ == '__main__':
-    game_idea()
+    game_idea_generate()
